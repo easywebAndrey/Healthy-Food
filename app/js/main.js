d7736707c4412acef4452811a780d916e5ace100
easywebAndrey <andreybondarenko1995@gmail.com>
2019-12-13 01:13:50 +0100

    finished block 'Sale' and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -33,12 +33,6 @@ $(document).ready(function () {
     }
   });
 
-  //popup organic
-  // $('.organic-box').magnificPopup({
-  //   delegate: 'img',
-  //   type: 'image'
-  // });
-
   //organic slider
   $('.tab-item').slick({
     infinite: true,
@@ -86,5 +80,59 @@ $(document).ready(function () {
   });
 
 
+  //timer
+  var timer;
+  var compareDate = new Date();
+  compareDate.setDate(compareDate.getDate() + 8); //just for this demo today + 7 days
+
+  timer = setInterval(function () {
+    timeBetweenDates(compareDate);
+  }, 1000);
+
+  function timeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if (difference <= 0) {
+
+      // Timer done
+      clearInterval(timer);
+
+    } else {
+
+      var seconds = Math.floor(difference / 1000);
+      var minutes = Math.floor(seconds / 60);
+      var hours = Math.floor(minutes / 60);
+      var days = Math.floor(hours / 24);
+
+      hours %= 24;
+      minutes %= 60;
+      seconds %= 60;
+
+      $(".days").text(days);
+      $(".hours").text(hours);
+      $(".minutes").text(minutes);
+      $(".seconds").text(seconds);
+    }
+  }
+
+  //sale-fruit
+  $('.sale-fruit__wrapper').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint: 522,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+  });
 
 });
